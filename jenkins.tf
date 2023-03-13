2e1bfc52518b95c7f6aebd841b67337a51742c24
root <root@ip-172-31-22-111.ec2.internal>
2023-03-13 00:06:27 +0000

    Add: Corrigir Erro

--- jenkins.tf
+++ jenkins.tf
@@ -8,3 +8,28 @@ module "jenkins_sg" {
   ingress_rules            = ["http-80-tcp"]
   egress_rules		   = ["all-all"]
 }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "jenkins-server"
+  ami                    = "ami-006dcf34c09e50022"
+  instance_type          = "t3.micro"
+  key_name               = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.jenkins_sg.security_group_id]
+  subnet_id              = "subnet-05f2f82fe398d0f1a"
+  user_data              = file("./dependencias.sh")
+  iam_instance_profile   = "LabInstanceProfile"
+
+  tags = {
+    Name   = "Jenkins-Server"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_eip" "jenkins-ip" {
+  instance = module.ec2_instance.id
+  vpc      = true
+}
